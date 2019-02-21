e7cfb21d76921804ed14c047dabd4b7438b210e8
falxala <satoyu96@gmail.com>
2019-02-21 21:59:14 +0900

    Update

---
+++ autoscroll.js
@@ -0,0 +1,12 @@
+$(function(){
+    $('#move_target').on('click', function(){
+        var targetTop = $('#target_point').offset().top;
+        //targetTopを表示
+        $('#target_top span').html(targetTop);
+
+        $('html,body').animate({
+            scrollTop: targetTop
+        }, 500);
+        return false;
+    });
+});
